4e91c03a62abc4211a53616abb7be7c17e2e58a7
uslessmty <597697242@qq.com>
2021-02-06 11:25:51 +0800

    Tree

--- Tree.js
+++ Tree.js
@@ -1,5 +1,6 @@
-export const Tree = function(val, left, right) {
+export const Tree = function(val, left, right, parent) {
   this.val = val || -1;
   this.left = left || null;
   this.right = right || null;
+  this.parent = parent || null;
 }
